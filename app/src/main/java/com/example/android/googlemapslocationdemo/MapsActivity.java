77b939923813da78dead7b75daaccfa912a71980
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2017-04-19 21:49:26 -0700

    added reverse geocoding

--- app/src/main/java/com/example/android/googlemapslocationdemo/MapsActivity.java
+++ app/src/main/java/com/example/android/googlemapslocationdemo/MapsActivity.java
@@ -3,6 +3,8 @@ package com.example.android.googlemapslocationdemo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,6 +24,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static android.R.id.list;
 import static android.os.Build.VERSION_CODES.M;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -82,6 +90,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.clear();
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+
+                // Reverse Geocoding
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if (listAddresses != null && listAddresses.size() > 0) {
+
+                        Log.i("Place Info", listAddresses.get(0).toString());
+
+                        String address = "";
+
+                        if (listAddresses.get(0).getSubThoroughfare() != null) {
+
+                            address += listAddresses.get(0).getSubThoroughfare() + ", ";
+
+                        }
+
+                        if (listAddresses.get(0).getThoroughfare() != null) {
+
+                            address += listAddresses.get(0).getThoroughfare() + ", ";
+
+                        }
+
+                        if (listAddresses.get(0).getLocality() != null) {
+
+                            address += listAddresses.get(0).getLocality() + ", ";
+
+                        }
+
+                        if (listAddresses.get(0).getPostalCode() != null) {
+
+                            address += listAddresses.get(0).getPostalCode() + ", ";
+
+                        }
+
+                        if (listAddresses.get(0).getCountryName() != null) {
+
+                            address += listAddresses.get(0).getCountryName() + " ";
+
+                        }
+
+                        Toast.makeText(MapsActivity.this, address, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
 
